eeff20e81ba916ba50dc77e0d4d314be16817c2a
loki6946 <aldianderson6@gmail.com>
2025-03-15 07:02:59 +0700

    change the book creator and book display logic

--- app.js
+++ app.js
@@ -13,70 +13,73 @@ class Book {
   }
 }
 
-function addBookToLibrary(book) {
-  library.push(book)
-  displayBook()
+function bookUiCreator(lib, currentBook) {
+  const book = document.createElement("div")
+  book.setAttribute("id", "book")
+  book.classList.add("book-container__book")
+
+  const bookRemoveContainer = document.createElement("div")
+  bookRemoveContainer.classList.add("book-container__book-remove-container")
+  const bookRemoveButton = document.createElement("div")
+  bookRemoveButton.classList.add("book-container__book-remove")
+  bookRemoveButton.addEventListener("click", () => {
+    book.remove()
+    const bookIndex = lib.indexOf(currentBook)
+    library.splice(bookIndex, 1)
+    displayBook()
+  })
+  const bookRemoveIcon = document.createElement("img")
+  bookRemoveIcon.setAttribute("src", "./assets/icons/close.svg")
+  bookRemoveIcon.setAttribute("alt", "remove-book-icon")
+  bookRemoveIcon.classList.add("book-container__book-remove-icon")
+  bookRemoveButton.appendChild(bookRemoveIcon)
+  bookRemoveContainer.appendChild(bookRemoveButton)
+
+  const bookInfoContainer = document.createElement("div")
+  bookInfoContainer.classList.add("book-container__book-info-container")
+  const bookTitle = document.createElement("h2")
+  bookTitle.textContent = currentBook.title
+  bookTitle.classList.add("book-container__book-title")
+  const bookAuthor = document.createElement("p")
+  bookAuthor.textContent = `Author: ${currentBook.author}`
+  bookAuthor.classList.add("book-container__book-author")
+  const bookPages = document.createElement("p")
+  bookPages.textContent = `Pages: ${currentBook.pages ? currentBook.pages : "Unknown"}`
+  bookPages.classList.add("book-container__book-pages")
+  bookInfoContainer.appendChild(bookTitle)
+  bookInfoContainer.appendChild(bookAuthor)
+  bookInfoContainer.appendChild(bookPages)
+
+  const bookStatusContainer = document.createElement("div")
+  bookStatusContainer.classList.add("book-container__book-status-container")
+  const bookStatusButton = document.createElement("button")
+  bookStatusButton.textContent = `Read: ${currentBook.read ? "Yes" : "No"}`
+  bookStatusButton.setAttribute("type", "button")
+  bookStatusButton.classList.add("book-container__book-status-toggle")
+  bookStatusButton.addEventListener("click", () => {
+    currentBook.readToggle()
+    bookStatusButton.textContent = `Read: ${currentBook.read ? "Yes" : "No"}`
+  })
+  bookStatusContainer.appendChild(bookStatusButton)
+
+  book.appendChild(bookRemoveContainer)
+  book.appendChild(bookInfoContainer)
+  book.appendChild(bookStatusContainer)
+  return book
 }
 
 function displayBook() {
   const bookContainer = document.querySelector("#book-container")
   bookContainer.querySelectorAll(`#book`).forEach((child) => child.remove())
   for (let i = 0; i < library.length; i++) {
-    const book = document.createElement("div")
-    book.setAttribute("id", "book")
-    book.classList.add("book-container__book")
-
-    const bookRemoveContainer = document.createElement("div")
-    bookRemoveContainer.classList.add("book-container__book-remove-container")
-    const bookRemoveButton = document.createElement("div")
-    bookRemoveButton.classList.add("book-container__book-remove")
-    bookRemoveButton.addEventListener("click", () => {
-      book.remove()
-      const bookIndex = library.indexOf(library[i])
-      library.splice(bookIndex, 1)
-      displayBook()
-    })
-    const bookRemoveIcon = document.createElement("img")
-    bookRemoveIcon.setAttribute("src", "./assets/icons/close.svg")
-    bookRemoveIcon.setAttribute("alt", "remove-book-icon")
-    bookRemoveIcon.classList.add("book-container__book-remove-icon")
-    bookRemoveButton.appendChild(bookRemoveIcon)
-    bookRemoveContainer.appendChild(bookRemoveButton)
-
-    const bookInfoContainer = document.createElement("div")
-    bookInfoContainer.classList.add("book-container__book-info-container")
-    const bookTitle = document.createElement("h2")
-    bookTitle.textContent = library[i].title
-    bookTitle.classList.add("book-container__book-title")
-    const bookAuthor = document.createElement("p")
-    bookAuthor.textContent = `Author: ${library[i].author}`
-    bookAuthor.classList.add("book-container__book-author")
-    const bookPages = document.createElement("p")
-    bookPages.textContent = `Pages: ${library[i].pages ? library[i].pages : "Unknown"}`
-    bookPages.classList.add("book-container__book-pages")
-    bookInfoContainer.appendChild(bookTitle)
-    bookInfoContainer.appendChild(bookAuthor)
-    bookInfoContainer.appendChild(bookPages)
-
-    const bookStatusContainer = document.createElement("div")
-    bookStatusContainer.classList.add("book-container__book-status-container")
-    const bookStatusButton = document.createElement("button")
-    bookStatusButton.textContent = `Read: ${library[i].read ? "Yes" : "No"}`
-    bookStatusButton.setAttribute("type", "button")
-    bookStatusButton.classList.add("book-container__book-status-toggle")
-    bookStatusButton.addEventListener("click", () => {
-      library[i].readToggle()
-      bookStatusButton.textContent = `Read: ${library[i].read ? "Yes" : "No"}`
-    })
-    bookStatusContainer.appendChild(bookStatusButton)
-
-    book.appendChild(bookRemoveContainer)
-    book.appendChild(bookInfoContainer)
-    book.appendChild(bookStatusContainer)
-    bookContainer.appendChild(book)
+    bookContainer.appendChild(bookUiCreator(library, library[i]))
   }
 }
 
+function addBookToLibrary(book) {
+  library.push(book)
+  displayBook()
+}
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true, library.length)
 addBookToLibrary(book1)
 
